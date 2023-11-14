f84b27a01c216ac275c74fb8a3a21cf677fab3dc
V11an <allankertich@gmail.com>
2023-11-14 19:18:54 +0000

    addition of calculator_1 file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 from calculator_1 import add(),sub(),mul(),div()
-
+a=10
+b=5
 print("a + b = " + str(add(a, b)) + "\n" 
         "a - b = " + str(sub(a, b)) + "\n"
         "a * b = " + str(mul(a, b)) + "\n"
