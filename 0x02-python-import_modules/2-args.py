bb89faec1fde6ab31cb25eff90e374ad7d111e66
V11an <allankertich@gmail.com>
2023-11-14 18:36:28 +0000

    a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+argv = input()
+
+def main(argv):
+
+    words = argv.split()
+    no_of_words = len(words)
+
+    if no_of_words == 0:
+        print(str(no_of_words) + " arguments.")
+    elif no_of_words == 1:
+        print(str(no_of_words) + " argument:\n" + str(no_of_words) + " : " + argv )
+    else:
+        print(str(no_of_words) + ": arguments  \n")
+        for no in words:
+            print(str(words.index(no)+1) + " : " + no)
+
+if __name__ == "__main__":
+    main(argv)
