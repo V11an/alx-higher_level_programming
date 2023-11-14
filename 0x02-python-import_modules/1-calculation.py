89000136940e0d638ba61f4460b5b8b44023823e
V11an <allankertich@gmail.com>
2023-11-14 21:23:11 +0000

    addition of calculator_1 file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
 from calculator_1 import add,sub,mul,div
-a=10
-b=5
+
+my_b = ["b=5"]
+my_a = ["a=10"]
+key_a,a_string = my_a[0].split('=')
+key_b,b_string = my_b[0].split('=')
+a = int(a_string)
+b = int(b_string)
+
 if __name__ == "__main__":
 
     print("{} + {} = {}".format(a,b,add(a,b)))
