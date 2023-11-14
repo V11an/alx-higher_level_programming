f1c99dd04e4ea66ea029ea6e9957d4674a0ff2cd
V11an <allankertich@gmail.com>
2023-11-14 19:03:26 +0000

    edit my file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 
-a = 10
-b = 5
+from calculator_1 import add(),sub(),mul(),div()
 
-def add(a, b):
-    return (a + b)
-def sub(a, b):
-    return(a - b)
-def mul(a, b):
-    return(a * b)
-def div(a, b):
-    return(a/b)
 print("a + b = " + str(add(a, b)) + "\n" 
         "a - b = " + str(sub(a, b)) + "\n"
         "a * b = " + str(mul(a, b)) + "\n"
