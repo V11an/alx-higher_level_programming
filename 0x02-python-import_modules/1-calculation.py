3b80baa211996704313b93e573faac78f60e02b7
V11an <allankertich@gmail.com>
2023-11-15 18:05:15 +0000

    infinite add

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,12 +6,12 @@ my_b = ["b=5"]
 my_a = ["a=10"]
 key_a,a_string = my_a[0].split('=')
 key_b,b_string = my_b[0].split('=')
-a = int(a_string)
-b = int(b_string)
+a = 10
+b = 5
 
 if __name__ == "__main__":
 
-    print("{} + {} = {}".format(a,b,add(a,b)))
-    print("{} + {} = {}".format(a,b,sub(a,b)))
-    print("{} + {} = {}".format(a,b,mul(a,b)))
-    print("{} + {} = {}".format(a,b,div(a,b)))
+    print("{a_string} + {b_string} = {}".format(add(a,b)))
+    print("{a_string} + {b_string} = {}".format(sub(a,b)))
+    print("{a_string} + {b_string} = {}".format(mul(a,b)))
+    print("{a_strinf} + {b_string} = {}".format(div(a,b)))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+values = input()
+
+def main(values):
+    separate_values = values.split()
+    no_of_values = len(separate_values)
+    total = 0
+    for i in separate_values:
+        total += eval(i)
+
+    print(total)
 
 if __name__ == "__main__":
-    import sys, math
-    result = 0
-    for i in sys.argv:
-        result += int(i)
-        print("{}".format(result))
+    main(values)
