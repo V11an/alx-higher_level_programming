d50b502569aac575179d830512c3bd8f47e95365
V11an <allankertich@gmail.com>
2023-11-15 18:46:19 +0000

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-value = str(input())
-values = value[:140]
+values = input()
 
 def main(values):
     separate_values = values.split()
-    no_of_values = len(separate_values)
     total = 0
     for i in separate_values:
-        total += eval(i)
+        total += int(i)
 
     print(total)
 
