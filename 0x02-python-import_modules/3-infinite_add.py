f7254a0cb25262ca48da8cb5ca66b18131bc5fe1
V11an <allankertich@gmail.com>
2023-11-15 19:07:20 +0000

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-try:
-    values = input()
-except EOFError as e:
-    print('\nEOFEerror')
-
-def main(values):
-    separate_values = values.split()
-    total = 0
-    for i in separate_values:
-        total += int(i)
-
-    print(total)
-
 if __name__ == "__main__":
-    main(values)
+
+    try:
+        values = input()
+        separate_values = values.split()
+        total = 0
+        for i in separate_values:
+            total += int(i)
+
+        print(total)
+    except EOFError as e:
+        print('\n error')
+
+
