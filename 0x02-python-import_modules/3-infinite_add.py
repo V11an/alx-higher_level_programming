9dd11d0582f5454e2931f2f5cbccdf18bf016268
V11an <allankertich@gmail.com>
2023-11-16 15:18:23 +0000

    corrections

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+
+import sys
+
+values = sys.argv[1:]
+def main(values):
+    total = 0
+    for i in values:
+        total += eval(i)
+
+    print(total)
+
 if __name__ == "__main__":
-
-    try:
-        values = map(int, input().split())
-        total = 0
-        for i in values:
-            total += int(i)
-
-        print(total)
-    except EOFError as e:
-        print(e)
-
-
+    main(values)
